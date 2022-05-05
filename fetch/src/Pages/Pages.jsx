0305bc932f5b67439662d88ecc14a544540dec8d
hadarsror <104713003+hadarsror@users.noreply.github.com>
2022-05-05 21:33:25 +0300

    hadars update

--- fetch/src/Pages/Pages.jsx
+++ fetch/src/Pages/Pages.jsx
@@ -2,13 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import Browse from "./Browse";
 import Chats from "./Chats";
 import Add from "./Add";
+import ActualChat from "./ActualChat";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import TouristOrLocal from "./TouristOrLocal";
 
 function Pages() {
   return (
     <Routes>
+      <Route path="/SignIn" element={<SignIn />} />
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/TouristOrLocal" element={<TouristOrLocal />} />
       <Route path="/Add" element={<Add />} />
-      <Route path="/" element={<Browse />} />
+      <Route path="/Browse" element={<Browse />} />
       <Route path="/Chats" element={<Chats />} />
+      <Route path="/ActualChat" element={<ActualChat />} />
     </Routes>
   );
 }
