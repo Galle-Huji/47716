47bf8ff2328a5807489e53dc14c0fabfbdc94a03
hadarsror <hadar.sror@mail.huji.ac.il>
2022-05-06 08:46:34 +0300

    hadar's

--- fetch/src/Components/FixedBottomNavigation.jsx
+++ fetch/src/Components/FixedBottomNavigation.jsx
@@ -38,7 +38,7 @@ export default function FixedBottomNavigation() {
             label="Browse"
             icon={<Groups />}
             component={Link}
-            to="/"
+            to="/Browse"
           />{" "}
           <BottomNavigationAction
             label="Chats"
