67431c07e08944a0206819caf2004110197f9bba
Inbar Karo <inbar.karo@mail.huji.ac.il>
2022-05-06 22:44:29 +0300

    user card is working!

--- fetch/src/Components/UserCards.jsx
+++ fetch/src/Components/UserCards.jsx
@@ -5,24 +5,21 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Button from '@mui/material/Button';
 import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import FaceIcon from '@mui/icons-material/Face';
+
 import CoffeeIcon from '@mui/icons-material/Coffee';
 import SportsBarIcon from '@mui/icons-material/SportsBar';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import { Grid } from '@mui/material';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
+
 
     
 const ExpandMore = styled((props) => {
@@ -47,17 +44,20 @@ export default function UserCards() {
   };
 
   return (
-    <Card sx={{ maxWidth: 300 }}>
-      <box>
+    <Card sx={{ maxWidth: 350 }}>
+      <box display="inline-block" gridTemplateColumns="repeat(auto-fill.50px)" gap={2} sx={{ justifyContent: "space-between"}}>
       <CardHeader
-        title="Erez Bitan"
+        title="Erez Segev"
         subheader="September 14, 2016"
       />
+      <Typography  mr={3}  sx={{marginTop: -8.5, float: "right", fontSize:'1.2rem', fontWeight: 'medium'}}>
+        24
+      </Typography>
       </box>
       <CardMedia
         component="img"
         height="194"
-        image="https://images.unsplash.com/photo-1601935093628-935710ebc41d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80"
+        image="https://cdn.stocksnap.io/img-thumbs/960w/confident-business_ZMIFYSYFK8.jpg"
         alt="user_pic"
           />
       <CardContent style={{ padding:7 }}>
@@ -73,27 +73,22 @@ export default function UserCards() {
           <Chip icon={<SportsBarIcon />} sx={{ m: 0.5 }} size="small" label="Beers" padding={10}/>
           <Chip icon={<FitnessCenterIcon />} sx={{ m: 0.5 }} size="small" label="Workout"/>
           <Chip icon={<AccountBalanceIcon />} sx={{ m: 0.5 }} size="small" label="Visit an attraction"/>
+          <Chip icon={<DinnerDiningIcon />} sx={{ m: 0.5 }} size="small" label="Eat out"/>
+          <Chip icon={<StorefrontIcon />} sx={{ m: 0.5 }} size="small" label="Visit a market"/>
+          <Chip icon={<ThumbUpIcon />} sx={{ m: 0.5 }} size="small" label="Fetch me for anything"/>
 
       </Box>
       </CardContent>
-
+      <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      mb = {2}
+      >
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-
-          <ExpandMoreIcon />
-        </ExpandMore>
+      <Button variant="outlined" align="center">Get To Know Me</Button>
       </CardActions>
+      </Box>
     </Card>
   );
 }
