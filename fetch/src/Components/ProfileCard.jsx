ee84b62bbf101df80ca353806207804cd67968b5
Gal Leshem <gal.leshem@mail.huji.ac.il>
2022-05-07 19:34:18 +0300

    Popup works with hard-codded data

--- fetch/src/Components/ProfileCard.jsx
+++ fetch/src/Components/ProfileCard.jsx
@@ -1,174 +1,126 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
+import React, { useEffect, useState } from "react";
 import Typography from "@mui/material/Typography";
-import PropTypes from "prop-types";
-import Avatar from "@mui/material/Avatar";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import { Paper, Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import Chip from "@mui/material/Chip";
+import CoffeeIcon from "@mui/icons-material/Coffee";
+import SportsBarIcon from "@mui/icons-material/SportsBar";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemText from "@mui/material/ListItemText";
-import DialogTitle from "@mui/material/DialogTitle";
-import Dialog from "@mui/material/Dialog";
-import PersonIcon from "@mui/icons-material/Person";
-import AddIcon from "@mui/icons-material/Add";
-import { blue } from "@mui/material/colors";
+import { Link } from "react-router-dom";
 
-const emails = ["username@gmail.com", "user02@gmail.com"];
+function ProfileCard() {
+  const [ProfilePictures, setProfilePictures] = useState([]);
+  useEffect(() => {
+    getProfilePictures();
+  }, []);
 
-function SimpleDialog(props) {
-  const { onClose, selectedValue, open } = props;
-
-  const handleClose = () => {
-    onClose(selectedValue);
-  };
-
-  const handleListItemClick = (value) => {
-    onClose(value);
+  const getProfilePictures = async () => {
+    const response = await fetch(`./FakeData/ProfilePictures.json`);
+    const data = await response.json();
+    setProfilePictures(data.ProfilePictures);
   };
 
   return (
-    <Dialog onClose={handleClose} open={open}>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          alt="green iguana"
-          height="140"
-          image="./FakeData/imgs/panitan-punpuang-G99Lrxmc-LA-unsplash.jpg"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Inbar
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Random Modal
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Gals are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        {/* <CardActions>
-          <Button size="small" onClick={handleClickOpen}>
-            Go to Profile
-          </Button>
-        </CardActions> */}
-        {/* <SimpleDialog
-          selectedValue={selectedValue}
-          open={open}
-          onClose={handleClose}
-        /> */}
-      </Card>
-      {/* <DialogTitle>Set backup account</DialogTitle>
-      <List sx={{ pt: 0 }}>
-        {emails.map((email) => (
-          <ListItem
-            button
-            onClick={() => handleListItemClick(email)}
-            key={email}
-          >
-            <ListItemAvatar>
-              <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-                <PersonIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={email} />
-          </ListItem>
+    <Card sx={{ maxWidth: 345 }}>
+      <Splide
+        aria-label="ProfilePictures"
+        options={{
+          rewind: true,
+        }}
+      >
+        {ProfilePictures.map((pic) => (
+          <SplideSlide>
+            <CardMedia
+              component="img"
+              alt={pic.PictureTitle}
+              height="140"
+              image={pic.Path}
+            />
+            <Typography
+              variant="h5"
+              sx={{
+                position: "absolute",
+                bottom: "10px",
+                left: "20px",
+                color: "White",
+                backgroundColor: "rgba(0, 0, 0, 0.2)",
+              }}
+            >
+              The Fetch Team
+            </Typography>
+          </SplideSlide>
         ))}
-
-        <ListItem
-          autoFocus
-          button
-          onClick={() => handleListItemClick("addAccount")}
+      </Splide>
+      <CardContent style={{ padding: 7 }}>
+        <Typography variant="body2" color="text.secondary">
+          <h3>About me:</h3>
+          <h4>Can’t seem to recall where I stole this bio from or why.</h4>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          <br />
+          <h3>Hobbies:</h3>
+        </Typography>
+        <Box
+          sx={{
+            margin: 0.5,
+            width: 0.999,
+            //  border: 1,
+            p: 1,
+            display: "block",
+          }}
         >
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Add account" />
-        </ListItem>
-      </List> */}
-    </Dialog>
-  );
-}
-
-SimpleDialog.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
-  selectedValue: PropTypes.string.isRequired,
-};
-
-export default function SimpleDialogDemo() {
-  const [open, setOpen] = React.useState(false);
-  const [selectedValue, setSelectedValue] = React.useState(emails[1]);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (value) => {
-    setOpen(false);
-    setSelectedValue(value);
-  };
-
-  return (
-    <div>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          alt="green iguana"
-          height="140"
-          image="./FakeData/avatars/gal.jpg"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Gal
-          </Typography>
+          <Chip icon={<CoffeeIcon />} size="small" label="Coffee" />
+          <Chip icon={<CoffeeIcon />} size="small" label="Coffee" />
+          <Chip icon={<CoffeeIcon />} size="small" label="Coffee" />
+          <Chip icon={<CoffeeIcon />} size="small" label="Coffee" />
+          <Chip icon={<CoffeeIcon />} size="small" label="Coffee" />
           <Typography variant="body2" color="text.secondary">
-            Gals are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            <br />
+            <h3>Fetches made happen:</h3>
           </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" onClick={handleClickOpen}>
-            Go to Profile
+          <List dense="True">
+            <ListItem>
+              <ListItemIcon>
+                <CoffeeIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText
+                primary={
+                  <Typography variant="body2" color="text.secondary">
+                    <h4>Fetched coffee with Yoav</h4>
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <SportsBarIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText
+                primary={
+                  <Typography variant="body2" color="text.secondary">
+                    <h4>Fetched beer with Inbar</h4>
+                  </Typography>
+                }
+              />
+            </ListItem>
+          </List>
+        </Box>
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <Button component={Link} to="/chats" variant="contained">
+            Fetch Chat!
           </Button>
-        </CardActions>
-        <SimpleDialog
-          selectedValue={selectedValue}
-          open={open}
-          onClose={handleClose}
-        />
-      </Card>
-    </div>
+        </Box>
+      </CardContent>
+    </Card>
   );
 }
 
-// export default function ImgMediaCard() {
-//   return (
-// <Card sx={{ maxWidth: 345 }}>
-//   <CardMedia
-//     component="img"
-//     alt="green iguana"
-//     height="140"
-//     image="./FakeData/avatars/gal.jpg"
-//   />
-//   <CardContent>
-//     <Typography gutterBottom variant="h5" component="div">
-//       Gal
-//     </Typography>
-//     <Typography variant="body2" color="text.secondary">
-//       Gals are a widespread group of squamate reptiles, with over 6,000
-//       species, ranging across all continents except Antarctica
-//     </Typography>
-//   </CardContent>
-//   <CardActions>
-//     <Button size="small">Go to Profile</Button>
-//   </CardActions>
-// </Card>
-//   );
-// }
+export default ProfileCard;
