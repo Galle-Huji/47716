ce589768c1d0bf2ac35bf1873fa79c7f612395f1
yoav carmon <yoav.carmon@mail.huji.ac.il>
2022-05-07 18:14:50 +0300

    cards json etc

--- fetch/src/Components/SingleChat.jsx
+++ fetch/src/Components/SingleChat.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ListItem from "@mui/material/ListItem";
+
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
+import { ListItemButton } from "@mui/material";
 
 function SingleChat({ ChatTitle, ChatMembers, LastMessege, ProfilePicture }) {
   return (
-    <ListItem alignItems="flex-start" button="true">
+    //checkout listitembutton api
+    <ListItemButton alignItems="flex-start">
       <ListItemAvatar>
         <Avatar alt="Profile Picture" src={ProfilePicture} />
       </ListItemAvatar>
@@ -27,7 +30,7 @@ function SingleChat({ ChatTitle, ChatMembers, LastMessege, ProfilePicture }) {
           </React.Fragment>
         }
       />
-    </ListItem>
+    </ListItemButton>
   );
 }
 
