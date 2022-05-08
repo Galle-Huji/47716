881df376098ef6dccd43450ac9376e5bbfa57202
Inbar Karo <inbar.karo@mail.huji.ac.il>
2022-05-09 01:10:45 +0300

    fgkgdfgdlk

--- fetch/src/Components/UserCards.jsx
+++ fetch/src/Components/UserCards.jsx
@@ -142,7 +142,7 @@ export default function UserCards({
         </CardContent>
         <Box display="flex" justifyContent="center" alignItems="center" mb={2}>
           <CardActions disableSpacing>
-            <Button align="center" onClick={handleClickOpen}>
+            <Button align="center" onClick={handleClickOpen} variant="contained">
               Get To Know Me
             </Button>
           </CardActions>
